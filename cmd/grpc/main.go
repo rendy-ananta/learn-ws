f6d855b9cc3e91c5cb1591fe53cbb4be9017aabc
Rendy Ananta <rendy.ananta@tokopedia.com>
2021-09-15 09:27:47 +0700

    add grpc server entrypoint

--- cmd/grpc/main.go
+++ cmd/grpc/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	taskServer "web-svc/grpc/task/server"
+)
 
 func main() {
-	fmt.Print("Hello grpc server")
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("cannot listening to the address: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	taskServer.RegisterTaskManagerServer(grpcServer, taskServer.TaskManagerServerImpl{})
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("cannot serve task grpc server: %v", err)
+	}
+
+	fmt.Println("grpc server started in :8080")
 }
